1fd9da86e3a5b8539845415f9d6db296ee17adf4
itsaiub <itsaiub@gmail.com>
2020-12-19 10:57:05 +0600

    bug fixed

--- mms_base/urls.py
+++ mms_base/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf.urls.static import static
 from mms_base import settings
-from accounts.views import HomeView, UserRegisterView
+from accounts.views import UserRegisterView
 from django.http import HttpResponse
 
 urlpatterns = [
